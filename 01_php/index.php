f3fe8903caf538d3134524f18bcdc28141c84f13
dilshan-shivantha <shiwanthadilshan3@gmail.com>
2024-02-20 00:59:06 +0530

    01. first php code

---
+++ 01_php/index.php
@@ -0,0 +1,19 @@
+<?php
+
+// the default file is the index.php
+echo 'Hello world!';
+
+//when using '\n' like below code this does not break the line
+echo ' HI I am Dilshan',"\n";
+echo ' HI I am Dilshan';
+
+// this is the way for create the line break
+echo nl2br("\n hi");
+echo "<h1>I am software engineer</h1>"
+
+?>
+
+<h1>
+    <?php echo "Statement two"; ?>
+</h1>
+
