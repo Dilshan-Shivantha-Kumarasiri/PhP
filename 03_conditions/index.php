4c84698ef97550287143e7f046a3e255ff0558bd
dilshan-shivantha <shiwanthadilshan3@gmail.com>
2024-02-20 01:00:50 +0530

    01. conditions

---
+++ 03_conditions/index.php
@@ -0,0 +1,16 @@
+<?php
+    $result = 6>5; //true
+    echo $result == true,"<br/>";
+
+    $first_name = "Dilshan";
+    $second_name = "Shivantha";
+
+    if ($first_name == "dilshan" && $second_name == "Shivantha"){
+        echo "The condition is true";
+    }else{
+        echo "The condition is false";
+    }
+
+
+
+?>
